065a61659713fba53ec20886d5fd3892252650b8
riza <rizaarifh338@gmail.com>
2019-09-04 02:06:49 +0700

    ketiga nambah spasi

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 </head>
 <body>
     <form action="back/proses_login.php" method="post">
-        <label for="username">Username:</label>
+        <label for="username">Username :</label>
         <input type="text" name="username" placeholder="Masukan Username">
     <form action="">
         <br>
-        <label for="username">Password:</label>
+        <label for="username">Password :</label>
         <input type="text" name="password" placeholder="password">
         <br>
     <input type="submit" value="Login">
